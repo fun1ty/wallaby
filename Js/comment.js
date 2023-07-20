097650db16324b4c98e3662cb8586be66e8d4868
saebyeok1 <qhdid3734@gmail.com>
2023-07-20 11:13:09 +0900

    feat: comment, like edit

--- Js/comment.js
+++ Js/comment.js
@@ -1,20 +1,48 @@
-const commentBox = document.querySelector("#commentBox");
+"use strict";
 
-// commentBox.addEventListener("submit", function (e) {
-//   e.preventDefault();
-//   const usernameInput = document.querySelector(".username");
-//   const commentInput = document.querySelector(".comment");
-//   console.log(usernameInput.value, commentInput.value);
-// });
+const postComment = () => {
+  const commentInput = document.getElementById("comment_input");
+  const commentPostBtn = document.getElementsByClassName("comment_btn")[0];
 
-const username = document.querySelector(".username");
-console.log(username);
+  // 댓글 입력시 요소 생성
+  const addNewComment = () => {
+    const newCommentLocation =
+      document.getElementsByClassName("comment_list")[0];
+    const newComment = document.createElement("li");
 
-const comment = commentBox.elements.comment.value;
+    newComment.innerHTML = `
+      <div class="user_desc">
+      <b>iAmUser</b>
+        <span>${commentInput.value}</span>
+        <button><i class="bi bi-suit-heart"></i></button>
+        </div>
+      </div>
+    `;
 
-const newComment = document.createElement("li");
-const bTag = document.createElement("b");
-bTag.append(username);
-newComment.append(bTag);
-newComment.append(`- ${comment}`);
-console.log(newComment);
+    newCommentLocation.appendChild(newComment);
+    commentInput.value = "";
+  };
+
+  // 사용자 입력 들어올 시, 게시 버튼 활성화
+  commentInput.addEventListener("keyup", () => {
+    commentInput.value
+      ? (commentPostBtn.style.opacity = "1")
+      : (commentPostBtn.style.opacity = "0.1");
+    if (window.event.keyCode === 13 && commentInput.value) {
+      addNewComment();
+    }
+  });
+
+  // 댓글 게시
+  commentPostBtn.addEventListener("click", () => {
+    if (commentInput.value) {
+      addNewComment();
+    } else {
+      alert("댓글이 입력되지 않았습니다 😳");
+    }
+  });
+};
+
+postComment();
+
+// https://habitual-history.tistory.com/entry/JS-westagram-%EB%8C%93%EA%B8%80-%EA%B2%8C%EC%8B%9C-%EA%B8%B0%EB%8A%A5-%EA%B5%AC%ED%98%84
