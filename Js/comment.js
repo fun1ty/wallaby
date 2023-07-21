0ff8337080dd7fc5c1d1298cd023b4ac68afc933
saebyeok1 <qhdid3734@gmail.com>
2023-07-21 10:56:28 +0900

    feat: comment edit

--- Js/comment.js
+++ Js/comment.js
@@ -12,15 +12,19 @@ const postComment = () => {
 
     newComment.innerHTML = `
       <div class="user_desc">
-      <b>iAmUser</b>
+        <span id="user_id"><b>user</b></span>
         <span>${commentInput.value}</span>
-        <button><i class="bi bi-suit-heart"></i></button>
-        </div>
+        <button class="like_btn"><i class="bi bi-suit-heart"></i></button>
+        <button class="reply_btn">답글</button>
       </div>
     `;
 
     newCommentLocation.appendChild(newComment);
     commentInput.value = "";
+
+    // 좋아요 버튼 클릭 이벤트 추가
+    const likeBtn = newComment.querySelector(".like_btn");
+    likeBtn.addEventListener("click", toggleLike);
   };
 
   // 사용자 입력 들어올 시, 게시 버튼 활성화
@@ -28,21 +32,88 @@ const postComment = () => {
     commentInput.value
       ? (commentPostBtn.style.opacity = "1")
       : (commentPostBtn.style.opacity = "0.1");
+    // enter 키로 댓글 입력
     if (window.event.keyCode === 13 && commentInput.value) {
       addNewComment();
+    } else if (window.event.keyCode == 13) {
+      alert("댓글이 입력되지 않았습니다 🥺");
     }
   });
 
-  // 댓글 게시
+  // 클릭으로 댓글 입력
   commentPostBtn.addEventListener("click", () => {
     if (commentInput.value) {
       addNewComment();
     } else {
-      alert("댓글이 입력되지 않았습니다 😳");
+      alert("댓글이 입력되지 않았습니다 🥺");
     }
   });
+
+  // 좋아요 버튼 토글 함수
+  const toggleLike = (e) => {
+    const heartIcon = e.currentTarget.querySelector("i");
+    const isFilled = heartIcon.classList.contains("bi-suit-heart-fill");
+
+    if (isFilled) {
+      heartIcon.classList.remove("bi-suit-heart-fill");
+      heartIcon.classList.add("bi-suit-heart");
+      heartIcon.style.color = "#000";
+    } else {
+      heartIcon.classList.add("bi-suit-heart-fill");
+      heartIcon.classList.remove("bi-suit-heart");
+      heartIcon.style.color = "#FF0000";
+    }
+  };
 };
 
 postComment();
 
-// https://habitual-history.tistory.com/entry/JS-westagram-%EB%8C%93%EA%B8%80-%EA%B2%8C%EC%8B%9C-%EA%B8%B0%EB%8A%A5-%EA%B5%AC%ED%98%84
+// 댓글 로컬스토리지에 저장 왜 안돼 씨
+
+document.getElementById("comment_wrap").onsubmit = function (e) {
+  e.preventDefault();
+
+  let name = document.getElementById("user_id").value;
+  let comment = document.getElementById("comment_input").value;
+
+  let commentSave = {
+    user: name,
+    content: comment,
+  };
+
+  let comments = getComment();
+
+  comments.push(commentSave);
+
+  localStorage.setItem("comments", JSON.stringify(comments));
+
+  loadComments();
+
+  document.getElementById("comment_wrap").reset();
+};
+
+function getComments() {
+  let comments = localStorage.getItem("comments");
+
+  if (comments) {
+    return JSON.parse(comments);
+  } else {
+    return [];
+  }
+}
+
+function loadComments() {
+  let commentList = document.getElementById("commentList");
+  commentList.innerHTML = "";
+
+  let comments = getComments();
+
+  for (let i = 0; i < comments.length; i++) {
+    let comment = comments[i];
+
+    let listItem = document.createElement("li");
+    listItem.innerHTML = `<strong>${comment.user}:</strong>+${comment.content}`;
+
+    commentList.appendChild(listItem);
+  }
+}
