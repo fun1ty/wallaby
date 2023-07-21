915e59c2b0c76442f6fc3df9e31007ffba16f02e
fun1ty <ettedlop@gmail.com>
2023-07-21 10:56:12 +0900

    20230721 중간merge

--- Js/board.js
+++ Js/board.js
@@ -8,8 +8,23 @@ $(document).ready(function () {
 
 // 게시물 보여주기
 function displayPost(post) {
-  var listItem = $("<li>").addClass("post").text(post.content);
-  $("#board_ul").append(listItem);
+  var listItem = document.createElement("li");
+  // 이미지와 내용이 모두 있는 경우에만 추가
+  const imageTag = post.img || "";
+  const content = post.content || "";
+  const date = post.date;
+  listItem.innerHTML = `${imageTag} <p>${content}</p><p class='time'>${date}</p>`;
+
+  // 이미지의 최대 너비와 높이를 200px로 설정
+  const images = listItem.querySelectorAll("img");
+  images.forEach((img) => {
+    img.style.maxWidth = "150px";
+    img.style.maxHeight = "150px";
+  });
+
+  $("#postList").append(listItem);
+  $("li").css({ display: "flex", "align-items": "center" });
+  $("p").css({ display: "block" });
 }
 
 function getPosts() {
